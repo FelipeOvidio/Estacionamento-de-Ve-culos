a8535f94ff480c2c6017d92d99dd4a99ca7104d7
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-22 01:40:22 -0300

    fix: rota de login de usuario

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { addUser, listUsers } = require('../controller/userControl');
+const { addUser, listUsers, loginUser } = require('../controller/userControl');
 const valdationRequest = require('../middleware/valdationRequest');
 const schemaUser = require('../schemas/userSchema');
+const validationLogin = require('../middleware/validationLogin');
 const userRouter = express();
 
 userRouter.post('/addUser', valdationRequest(schemaUser), addUser)
+userRouter.post('/login', validationLogin, loginUser)
 userRouter.get('/listUsers', listUsers)
 
 
