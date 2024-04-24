b601e9aaade6d99f23e8d3132f69130eb7d5944a
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-23 21:49:36 -0300

    fix: Recurso de atulização de dados do usuário

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addUser, listUsers, loginUser } = require('../controller/userControl');
+const { addUser, listUsers, loginUser, updateUser } = require('../controller/userControl');
 const valdationRequest = require('../middleware/valdationRequest');
 const schemaUser = require('../schemas/userSchema');
 const validationLogin = require('../middleware/validationLogin');
@@ -7,6 +7,7 @@ const userRouter = express();
 
 userRouter.post('/addUser', valdationRequest(schemaUser), addUser)
 userRouter.post('/login', validationLogin, loginUser)
+userRouter.put('/updateUser/:id', updateUser)
 userRouter.get('/listUsers', listUsers)
 
 
