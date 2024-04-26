1f9431876327a2fbd7640631ff1831c99f9a62ab
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-25 23:04:15 -0300

    fix: Rota para excluir um usuario pelo id

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addUser, listUsers, loginUser, updateUser } = require('../controller/userControl');
+const { addUser, listUsers, loginUser, updateUser, deleteuser } = require('../controller/userControl');
 const valdationRequest = require('../middleware/valdationRequest');
 const schemaUser = require('../schemas/userSchema');
 const validationLogin = require('../middleware/validationLogin');
@@ -7,8 +7,10 @@ const userRouter = express();
 
 userRouter.post('/addUser', valdationRequest(schemaUser), addUser)
 userRouter.post('/login', validationLogin, loginUser)
-userRouter.put('/updateUser/:id', updateUser)
 userRouter.get('/listUsers', listUsers)
+userRouter.put('/updateUser/:id', updateUser)
+userRouter.delete('/deleteUser/:id', deleteuser)
+
 
 
 module.exports = userRouter
