a1b1b309bd5c9562c74f8b4bec78b1dd788a79b2
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-30 06:52:01 -0300

    fix: Configuração parar usar os recursos dos clientes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const knex = require('./database/config')
 const express = require('express');
 const userRouter = require('./router/userRouter');
+const clientRouter = require('./router/clientRouter');
 const app = express();
 
 app.use(express.json());
 
 app.use(userRouter)
+app.use(clientRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Serv Start");
