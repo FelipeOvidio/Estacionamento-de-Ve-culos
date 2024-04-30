89a018e1231cfb78d4146b52692a86f3cd9c7328
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-30 10:29:30 -0300

    fix: correção de código

--- src/controller/clientControl.js
+++ src/controller/clientControl.js
@@ -1,37 +1,38 @@
 const knex = require('../database/config')
 
+const customers = async (req, res) => {
+    try {
+        const listCostumers = await knex.select('*').from('clientes')
+        if (listCostumers.length == 0) {
+            return res.status(404).json({ msg: 'Não clientes cadastrados' })
+        }
+        return res.status(200).json(listCostumers)
+
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+
+    }
+}
+
 const addClient = async (req, res) => {
     const { name, cpf, telephone } = req.body
     try {
-        const existCpf = await knex('usuarios').where({ cpf }).first()
-        if (existCpf) {
-            return res.status(409).json({ msg: 'Cpf ja possui cadastro' })
-        }
         const newClient = {
             name,
             cpf,
             telephone
         }
-        const client = await knex('clientes').insert(newClient)
+        const dataClient = await knex('clientes').insert(newClient)
+
         return res.status(201).json({ msg: 'Cliente cadastrado com sucesso' })
+
     } catch (error) {
         return res.status(500).json({ msg: error.message })
     }
 }
 
-const listCustomers = async (res, req) => {
-    try {
-        const customers = select('id', 'name', 'cpf', 'telephone').from('clientes').orderBy('id')
-        if (customers.length === 0) {
-            return res.status(400).json({ msg: "Não há usuario cadastrado" })
-        }
-        return res.status(200).json(customers)
-    } catch (error) {
-        return res.status(500).json({ msg: error.message })
-    }
-}
 
 module.exports = {
-    addClient,
-    listCustomers
+    customers,
+    addClient
 }
