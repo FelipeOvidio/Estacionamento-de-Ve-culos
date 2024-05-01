1d2fb2c6f7b7147f386a05db549e2c74c6f0d7f6
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-30 23:42:45 -0300

    fix: Correção no código

--- src/middleware/valdationRequest.js
+++ src/middleware/valdationRequest.js
@@ -4,7 +4,7 @@ const valdationRequest = joiSchema => async (req, res, next) => {
         next()
 
     } catch (error) {
-        console.log(error);
+
         return res.status(500).json({ msg: error.message })
     }
 
