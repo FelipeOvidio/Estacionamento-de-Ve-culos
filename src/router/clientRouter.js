0159798c50f46efdce2ecd63ff3f6551e3d1cb2f
FelipeOvidio <f.costaovidio@gmail.com>
2024-04-30 23:44:20 -0300

    feat: inserindo middleware na rota de cadastro

--- src/router/clientRouter.js
+++ src/router/clientRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { customers, addClient } = require('../controller/clientControl');
+const valdationRequest = require('../middleware/valdationRequest');
+const schemaClient = require('../schemas/clientSchema');
 
 
 
 const clientRouter = express();
 
 clientRouter.get('/customers', customers)
-clientRouter.post('/addClient', addClient)
+clientRouter.post('/addClient', valdationRequest(schemaClient), addClient)
 
 module.exports = clientRouter
