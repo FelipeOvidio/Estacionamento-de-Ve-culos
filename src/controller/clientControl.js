0edeb0100e9a32a2b4f90c083401cc411b761217
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-01 12:00:12 -0300

    feat: Recurso de atulização dos dados do cliente

--- src/controller/clientControl.js
+++ src/controller/clientControl.js
@@ -2,7 +2,7 @@ const knex = require('../database/config')
 
 const customers = async (req, res) => {
     try {
-        const listCostumers = await knex.select('*').from('clientes')
+        const listCostumers = await knex.select('*').from('clientes').orderBy('id')
         if (listCostumers.length == 0) {
             return res.status(404).json({ msg: 'Não clientes cadastrados' })
         }
@@ -17,6 +17,14 @@ const customers = async (req, res) => {
 const addClient = async (req, res) => {
     const { name, cpf, telephone } = req.body
     try {
+        const existCpf = await knex('clientes').where({ cpf }).first()
+        if (existCpf) {
+            return res.status(409).json({ msg: 'Ja existe cadastro com CPF informado' })
+        }
+        const existTell = await knex('clientes').where({ telephone }).first()
+        if (existTell) {
+            return res.status(409).json({ msg: 'Telefone informado não pode ser cadastrado' })
+        }
         const newClient = {
             name,
             cpf,
@@ -31,8 +39,26 @@ const addClient = async (req, res) => {
     }
 }
 
+const updateClient = async (req, res) => {
+    const { name, cpf, telephone } = req.body
+    const { id } = req.params
+    try {
+
+        const client = await knex('clientes').update({ name, cpf, telephone }).where({ id })
+        if (client) {
+            return res.status(200).json({ msg: 'Dados atulizado com sucesso' })
+        }
+
+        return res.status(404).json({ msg: 'Não existe usuario com o Id informado' })
+
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+    }
+}
+
 
 module.exports = {
     customers,
-    addClient
+    addClient,
+    updateClient
 }
