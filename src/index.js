80d1e8de23c1b27fca53457b9e22a5b056db7d4d
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-01 23:43:52 -0300

    fix: Configurando rotas do veiculos

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const knex = require('./database/config')
 const express = require('express');
 const userRouter = require('./router/userRouter');
 const clientRouter = require('./router/clientRouter');
+const vehiclesRouter = require('./router/vehiclesRouter');
 const app = express();
 
 app.use(express.json());
 
 app.use(userRouter)
 app.use(clientRouter)
+app.use(vehiclesRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Serv Start");
