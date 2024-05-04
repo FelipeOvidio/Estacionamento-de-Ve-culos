1cd2af43691f680c4700361cd20e4b67743a899f
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-03 22:22:44 -0300

    feat: Esquema de validação de campos de cadastro de veiculos

---
+++ src/schemas/vehicleSchema.js
@@ -0,0 +1,22 @@
+const joi = require('joi')
+
+
+const vehicleSchema = joi.object({
+    brand: joi.string().required().messages({
+        'any.required': 'Informe a marca do veículo'
+    }),
+    model: joi.string().required().messages({
+        'any.required': 'Infome o modelo do veículo'
+    }),
+    plate: joi.string().required().messages({
+        'any.required': 'Informe a placa do veiculo'
+    }),
+    year_vehicle: joi.number().required().messages({
+        'any.required': 'Informe o ano do veiculo'
+    }),
+    client_id: joi.number().required().messages({
+        'any.required': 'Informe o ID do proprietário do veículo'
+    })
+})
+
+module.exports = vehicleSchema
