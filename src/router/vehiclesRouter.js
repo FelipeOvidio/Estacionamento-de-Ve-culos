5c839b0a1536f42045fdcbd84bc101a60224474d
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-03 22:23:54 -0300

    feat: Middleware de validação de request

--- src/router/vehiclesRouter.js
+++ src/router/vehiclesRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { listCars, addCar } = require('../controller/vehiclesControl')
+const valdationRequest = require('../middleware/valdationRequest')
+const vehicleSchema = require('../schemas/vehicleSchema')
 const vehiclesRouter = express()
 
 vehiclesRouter.get('/vehiclesList', listCars)
-vehiclesRouter.post('/addVehicle', addCar)
+vehiclesRouter.post('/addVehicle', valdationRequest(vehicleSchema), addCar)
 
 module.exports = vehiclesRouter
