7da67a954545c8dd01b57a46ef0ec19298391a51
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-03 22:22:16 -0300

    feat: Inserindo ano de veiculo

--- src/controller/vehiclesControl.js
+++ src/controller/vehiclesControl.js
@@ -13,13 +13,18 @@ const listCars = async (req, res) => {
 }
 
 const addCar = async (req, res) => {
-    const { brand, model, plate, cliente_id } = req.body
+    const { brand, model, plate, year_vehicle, client_id } = req.body
     try {
         const dataCar = {
             brand,
             model,
             plate,
-            cliente_id
+            year_vehicle,
+            client_id
+        }
+        const existPlate = await knex('veiculos').where({ plate }).first()
+        if (existPlate) {
+            return res.status(409).json({ msg: 'Placa ja possui cadastro' })
         }
         const newCar = await knex('veiculos').insert(dataCar)
         return res.status(201).json({ msg: 'Veiculo cadastrado com sucesso' })
