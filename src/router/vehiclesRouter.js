e6337a989b44348b10276e56393d5a2c9a2040a8
FelipeOvidio <f.costaovidio@gmail.com>
2024-05-05 00:29:50 -0300

    feat: Rota para recurso de manipulação dos dados do veiculo

--- src/router/vehiclesRouter.js
+++ src/router/vehiclesRouter.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { listCars, addCar } = require('../controller/vehiclesControl')
+const { listCars, addCar, exitVehicle, registerVehicles } = require('../controller/vehiclesControl')
 const valdationRequest = require('../middleware/valdationRequest')
 const vehicleSchema = require('../schemas/vehicleSchema')
+const registerExit = require('../schemas/vehicleSchema')
 const vehiclesRouter = express()
 
 vehiclesRouter.get('/vehiclesList', listCars)
+vehiclesRouter.get('/registerVehicles', registerVehicles)
 vehiclesRouter.post('/addVehicle', valdationRequest(vehicleSchema), addCar)
+vehiclesRouter.delete('/exitVehicle/:id', valdationRequest(registerExit), exitVehicle)
 
 module.exports = vehiclesRouter
